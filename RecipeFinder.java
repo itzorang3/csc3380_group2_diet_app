76563ed9bc088e9909dc8db8f1f46227934f98ca
JungJiwon1234 <jjung17@lsu.edu>
2025-11-03 12:29:59 -0600

    Add files via upload
    READ ME
    Instructions: Enter your ingredients and format as the instructions say when you run on compiler.
    Libraries: Uses only java.util but specifically uses java.util.Scanner, java.util.HashSet, java.util.HashMap, and java.util.ArrayList.

---
+++ RecipeFinder.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class RecipeFinder {
+
+    public static HashSet<String> scanner() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        HashSet<String> Ingredients = new HashSet<>(Arrays.asList(input.split(",")));
+
+        scanner.close();
+        return Ingredients;
+    }
+
+    public static void Question() {
+        System.out.println("SEPARATE THE INGREDIENTS WITH COMMAS & ONLY USE SPACES FOR SPECIFIC WORDS");
+        System.out.println("e.g. pickles,pickled ginger,apples");
+        System.out.println("Enter the ingredients: ");
+    }
+
+    public static HashMap<String, Set<String>> Recipes() {
+        HashMap<String, Set<String>> Recipes = new HashMap<>();
+        Recipes.put("Peanut Butter Sandwich", new HashSet<>(Arrays.asList("bread", "peanut butter", "jelly")));
+        Recipes.put("Ham Cheese Sandwich", new HashSet<>(Arrays.asList("ham", "cheese", "bread")));
+        Recipes.put("Pan Fried Salmon", new HashSet<>(Arrays.asList("butter", "salmon", "garlic")));
+
+        return Recipes;
+    }
+
+    public static HashSet<String> BestRecipes(HashSet<String> Ingredients, HashMap<String, Set<String>> Recipes) {
+        HashSet<String> BestRecipes = new HashSet<>();
+        
+        for (Map.Entry<String, Set<String>> entry : Recipes.entrySet()) {
+        Set<String> temp = new HashSet<>(entry.getValue());
+        temp.retainAll(Ingredients);
+
+        if (temp.size() > 0) {
+            BestRecipes.add(entry.getKey());
+        }
+    }
+
+
+        return BestRecipes;
+    }
+
+
+    public static void main(String[] args) {
+
+        RecipeFinder.Question();
+
+        HashSet<String> Ingredients = scanner();
+
+        HashSet<String> best = BestRecipes(Ingredients, Recipes());
+        System.out.println("Recipes you can make with your ingredients: " + best);
+ 
+    }
+
+}
+
+
